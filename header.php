0b09682670c40d161144ac588ba64efca9a42491
FranBo10 <franbo10pro@gmail.com>
2022-10-15 15:43:17 +0200

    Tercera fase la_bolata

---
+++ header.php
@@ -0,0 +1,51 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="document" content="cerveza, billar, billares, dardos, conciertos, concierto, barra, terraza, aire libre">
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Oswald:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_url')?>\assets\css\Progrid.css"> 
+  
+  <title>La Bolata</title>
+
+</head>
+
+<body>
+  <div class="container grid-explicit">
+    <header>
+      <div class="header__principal" id="grad">
+        <div class="logo">
+          <img src="C:\Users\fborj\OneDrive\Escritorio\LA BOLATA\bolata.jpg">
+        </div>
+        <div class="menu__principal">
+        <div class="menu">
+        <nav>
+            <ul>
+              <li><a href="#" id="selected
+                ">Inicio</a></li>
+              <li><a href="#">Eventos</a>
+                <ul>
+                  <li><a href="#">Conciertos</a></li>
+                  <li><a href="#">Reserva el local</a></li>
+                  <li><a href="#">Juegos</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Carteles</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Contacto</a></li>
+            </ul>
+          </nav>
+        </div>      
+        <div class="search">
+          <input type="search" placeholder="Que buscas ?">
+          <i class="fas fa-search"></i>
+        </div>
+        </div>
+      </div>
+      
+    </header>
---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<aside class="item-sidebar-1">
+              <p>Sidebar</p>
+          </aside>
+        <aside class="item-sidebar-2">
+              <p>Sidebar</p>
+          </aside>
+        <article class="item-content">
+              <img src="C:\Users\fborj\OneDrive\Escritorio\LA BOLATA\content.jpeg">
+          </article>
